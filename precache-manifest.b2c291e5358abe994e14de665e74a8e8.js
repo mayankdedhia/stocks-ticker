5d0107202e7334eec00ca088f106830d0466f944
Mayank Dedhia <mayank.dedhia@systumbdi.com>
2020-09-27 20:01:51 +0530

    Updates

---
+++ precache-manifest.b2c291e5358abe994e14de665e74a8e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d15e185e50668191d36531a729fe6256",
+    "url": "/stocks-ticker/index.html"
+  },
+  {
+    "revision": "fe9fbc6e1907826ab8ef",
+    "url": "/stocks-ticker/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "81793afdf063d3e16746",
+    "url": "/stocks-ticker/static/js/2.0d05aaf2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stocks-ticker/static/js/2.0d05aaf2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe9fbc6e1907826ab8ef",
+    "url": "/stocks-ticker/static/js/main.a7bcfd8b.chunk.js"
+  },
+  {
+    "revision": "7ac7e7de509f2c3f0acf",
+    "url": "/stocks-ticker/static/js/runtime-main.7708b764.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/stocks-ticker/static/media/logo.5d5d9eef.svg"
+  }
+]);
