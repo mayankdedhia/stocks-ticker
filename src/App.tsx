134b68c3562713bc8a1d93ea09335f4933b21fc9
Mayank Dedhia <mayank.dedhia@systumbdi.com>
2020-09-27 23:15:36 +0530

    Add web socket to load stock data

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState, useEffect, useRef } from "react";
 import "./App.css";
 
+const tickerURL = "ws://stocks.mnet.website/";
+
 function App() {
+  const [isTickerLive, setTickerStatus] = useState(false);
+  const connection = useRef<WebSocket | null>(null);
+
+  useEffect(() => {
+    connection.current = new WebSocket(tickerURL);
+    connection.current.onopen = () => {
+      setTickerStatus(true);
+    };
+    connection.current.onmessage = (message) => {
+      console.log(message.data);
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log(`Ticker status changed : ${isTickerLive}`);
+    if (isTickerLive) {
+      const container = document.getElementById("container");
+      if (container != null) {
+        container.innerHTML = "Show table here";
+      }
+    }
+  }, [isTickerLive]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="container" className="app-body">
+        <div>Connecting to the stock ticker</div>
+      </div>
     </div>
   );
 }
